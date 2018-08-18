4f0024a7242828ff654e0058c09090cd426620f4
Jasmine Iwanek <jasmine@iwanek.co.uk>
2018-08-17 22:52:13 -0700

    update mappings to stable_29

--- src/main/java/com/knoxhack/energyarsenal/item/ItemRFSword.java
+++ src/main/java/com/knoxhack/energyarsenal/item/ItemRFSword.java
@@ -103,8 +103,8 @@ public class ItemRFSword extends ItemRF {
 
         if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
         {
-            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.attackDamage, 0));
-            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.4000000953674316D, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.attackDamage, 0));
+            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.4000000953674316D, 0));
         }
 
         return multimap;
@@ -157,8 +157,5 @@ public class ItemRFSword extends ItemRF {
         {
             return super.getStrVsBlock(stack, state);
         }
-    }}
-
-	
-
-
+    }
+}
